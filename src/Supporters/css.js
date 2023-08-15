64ad263b821d91c6646108886f10b407b96a7ea9
Salillakra <salillakra223@gmail.com>
2023-08-15 07:39:09 +0530

    fixed error

--- src/Supporters/css.js
+++ src/Supporters/css.js
@@ -1,6 +1,6 @@
 export const bgStyle = {
   backgroundImage:
-    "url(https://source.unsplash.com/random/1000x1200/?todolist)",
+    "url(https://source.unsplash.com/random/1000x1200/?wallpaper)",
   backgroundSize: "cover",
   backgroundRepeat: "no-repeat",
   backgroundPosition: "center",
